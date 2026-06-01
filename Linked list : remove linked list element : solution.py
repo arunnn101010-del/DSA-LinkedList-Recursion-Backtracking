87628e93c44bde43465132a80d3556e6518c8974
Arun <arunnn101010@gmail.com>
2026-06-01 21:43:15 +0530

    Create Linked list : remove linked list element : solution.py

---
+++ Linked list : remove linked list element : solution.py
@@ -0,0 +1,31 @@
+# Promblem - remove linked list element 
+# Appraoch - linked list traversal + dummy LL
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 203 & easy 
+class Solution {
+public:
+
+    ListNode* removeElements(ListNode* head, int val) {
+
+        ListNode dummy(0);
+
+        dummy.next = head;
+
+        ListNode* curr = &dummy;
+
+        while(curr->next) {
+
+            if(curr->next->val == val) {
+
+                curr->next = curr->next->next;
+            }
+
+            else {
+
+                curr = curr->next;
+            }
+        }
+
+        return dummy.next;
+    }
+};
