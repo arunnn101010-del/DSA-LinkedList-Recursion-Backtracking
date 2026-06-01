83f33be8ec02cfcf93d4411c842be5a8cdd254d7
Arun <arunnn101010@gmail.com>
2026-06-01 20:58:12 +0530

    Update Linked list : linked list cycle : solution.py

--- Linked list : linked list cycle : solution.py
+++ Linked list : linked list cycle : solution.py
@@ -1,1 +1,21 @@
+# Promblem - linked list cycle 
+# Appraoch - cycle detection & two pointers
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 141 & easy 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
