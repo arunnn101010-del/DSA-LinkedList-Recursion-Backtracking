09868c24d8732ffdb79acfcfa2f4bf904a2177c0
Arun <arunnn101010@gmail.com>
2026-06-03 22:14:51 +0530

    Update Linked List : middle of the linked list : solution.py

--- Linked List : middle of the linked list : solution.py
+++ Linked List : middle of the linked list : solution.py
@@ -1,1 +1,17 @@
+# Promblem - middle of the linked list 
+# Approach - Two pointers
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 876 & easy 
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
 
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
