b50f3396f68bd6f55cdc339dd4c85db3faaa3a87
Arun <arunnn101010@gmail.com>
2026-06-03 22:18:02 +0530

    Update Linked list : intersection of two linked list : solution.py

--- Linked list : intersection of two linked list : solution.py
+++ Linked list : intersection of two linked list : solution.py
@@ -1,1 +1,17 @@
+# Promblem - intersection of two linked list 
+# Approach - two pointers
+# Time and space complexity - 0(n+m) & 0(1)
+# Leetcode and diffculty level - 160 & easy 
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* pA = headA;
+        ListNode* pB = headB;
 
+        while(pA != pB) {
+            pA = (pA == NULL) ? headB : pA->next;
+            pB = (pB == NULL) ? headA : pB->next;
+        }
+        return pA;
+    }
+};
