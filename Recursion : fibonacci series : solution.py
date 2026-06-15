0e2bafed7c0d4634cbf6c26abb4a7420d4bac125
Arun <arunnn101010@gmail.com>
2026-06-15 23:27:12 +0530

    Update Recursion : fibonacci series : solution.py

--- Recursion : fibonacci series : solution.py
+++ Recursion : fibonacci series : solution.py
@@ -1,1 +1,20 @@
+# Promblem - fibonacci series 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 509 & easy 
+class Solution {
+public:
+    int fib(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        int prev2 = 0; 
+        int prev1 = 1;
 
+        for(int i=2; i<=n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
