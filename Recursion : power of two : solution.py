7a0df8c3d066cf5eec24a6438826638eab553eb7
Arun <arunnn101010@gmail.com>
2026-06-16 23:25:15 +0530

    Update Recursion : power of two : solution.py

--- Recursion : power of two : solution.py
+++ Recursion : power of two : solution.py
@@ -1,1 +1,12 @@
-
+# Promblem - power of two 
+# Time and space complexity - 0(1) & 0(1) 
+# Leetcode and diffculty level - 231 & easy 
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+};
