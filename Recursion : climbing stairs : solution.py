43e6897a541e49bf88577a226259e17a26083d9d
Arun <arunnn101010@gmail.com>
2026-06-16 22:59:52 +0530

    Update Recursion : climbing stairs : solution.py

--- Recursion : climbing stairs : solution.py
+++ Recursion : climbing stairs : solution.py
@@ -1,1 +1,23 @@
+# Promblem - climbing stairs 
+# Appraoch - dyanmic proagramming 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffculty level - 70 & easy 
+class Solution {
+public:
+    int climbStairs(int n) {
+        
+        if(n <= 2) 
+            return n;
 
+        int prev2 = 1;
+        int prev1 = 2;
+
+        for(int i=3; i <= n; i++) {
+            int curr = prev1 + prev2;
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
