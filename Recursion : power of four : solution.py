df8073c4dabb509351f21e8527bb7f07431461fe
Arun <arunnn101010@gmail.com>
2026-06-18 23:12:32 +0530

    Update Recursion : power of four : solution.py

--- Recursion : power of four : solution.py
+++ Recursion : power of four : solution.py
@@ -1,1 +1,13 @@
-
+# Promblem - power of four 
+# Approach - bit manuplation 
+# Time and space complexity - 0(n) & 0(1)
+# Leetcode and diffcuilty level - 342 & easy 
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if( n <=0) {
+            return false;
+        }
+        return (n & (n-1)) == 0 && (n & 0x55555555) != 0;
+    }
+};
