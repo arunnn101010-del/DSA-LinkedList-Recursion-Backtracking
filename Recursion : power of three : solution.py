d6bfdada82cf60c4be37f069957b3d1fd5e6e4f6
Arun <arunnn101010@gmail.com>
2026-06-18 22:57:10 +0530

    Implement power of three check in solution.py
    Added a solution to check if a number is a power of three using division.

--- Recursion : power of three : solution.py
+++ Recursion : power of three : solution.py
@@ -1,1 +1,16 @@
-
+# Promblem - power of three 
+# Approach - division 
+# Time and space complexity - 0(log3 n) & 0(1)
+# Leetcode and diffculty level - 326 & easy 
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        while(n % 3 == 0) {
+            n = n / 3;
+        }
+        return n == 1;
+    }
+};
